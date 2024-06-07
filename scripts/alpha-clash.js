ef7b2ad836d8860c88d7a37ea8af6860102dd4f6
Shohan Sharoar <maffin.killer007@gmail.com>
2024-06-08 03:06:53 +0600

    Build GetRandom Alphabet Function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,15 @@
+function continueGame(){
+    const alphabets = getRandomAlphabet();
+    
+}
+
+
+
 function play(){
     // Getting the home section and hide it
     hideElementById('home-screen');
 
     // Showing the play ground section
     showElementById('play-ground');
+    continueGame();
 }
