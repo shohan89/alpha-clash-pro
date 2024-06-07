359edd6a1996f77f5773906723ca33a9e574ba15
Shohan Sharoar <maffin.killer007@gmail.com>
2024-06-08 02:49:54 +0600

    Change Home Screen To Playground By Click

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,7 @@
 function play(){
-    console.log('Clicked!!');
+    // Getting the home section and hide it
+    hideElementById('home-screen');
+
+    // Showing the play ground section
+    showElementById('play-ground');
 }
